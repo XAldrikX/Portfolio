83043e4ea99ffb7674ae19837bdc55f2cc8e938f
XAldrikX <nicolasbortoli2010@hotmail.com>
2022-11-14 22:15:05 -0300

    projects json

--- src/views/PortfolioView.vue
+++ src/views/PortfolioView.vue
@@ -1,3 +1,39 @@
 <template>
-    <h1>This is the portfolio page</h1>
+    <main>
+        <section v-for="project in data.projects" :key="project.id" class="project">
+            <div class="left" v-if="project.position === 'left'">
+                <img class="left-img" :src='project.img' alt="website with charts, metrics and alikes">
+                <h1 class="left-text">{{ project.title }}</h1>
+                <p>{{ project.description }}</p>
+            </div>
+
+            <div class="right" v-else>
+                <h1 class="right-text">{{ project.title }}</h1>
+                <img class="right-img" :src="project.img" alt="website with charts, metrics and alikes">
+                <p>{{ project.description }}</p>
+            </div>
+        </section>
+    </main>
 </template>
+
+<script>
+    import projects from '../../src/projects.json';
+
+    export default {
+        name: "PortfolioView",
+        data() {
+            return {
+                data: projects,
+            }
+        }
+    }
+</script>
+
+<style scoped>
+    .left {
+        color: red;
+    }
+    .right {
+        color: blue;
+    }
+</style>
