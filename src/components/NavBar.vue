6c16b704586cb9ef0b12b212bbf977ea0db671da
XAldrikX <nicolasbortoli2010@hotmail.com>
2023-02-12 21:17:50 -0300

    Menu mobile

--- src/components/NavBar.vue
+++ src/components/NavBar.vue
@@ -1,16 +1,50 @@
 <template>
     <header>
-        <nav class="nav-wrapper">
-            <router-link to="/">Home</router-link>
-            <router-link to="/portfolio">Portfolio</router-link>
-            <router-link to="/contact">Contact me</router-link>
-        </nav>
+        <button class="mobile-menu-btn" @click="open_close_menu()">
+            <Menu v-if="!menu" />
+            <Close v-else />
+        </button>
+        <div class="mobile-menu" v-show="menu">
+            <nav class="nav-wrapper">
+                <router-link to="/">Home</router-link>
+                <router-link to="/portfolio">Portfolio</router-link>
+                <router-link to="/contact">Contact me</router-link>
+            </nav>
+        </div>
     </header>
 </template>
 
 <script>
+    import Menu from '../components/icons/Menu.vue';
+    import Close from '../components/icons/Close.vue'
+
     export default {
         name: 'NavBar',
+        components: {
+            Menu,
+            Close
+        },
+        data() {
+            return {
+                menu: false,
+            }
+        },
+        methods: {
+            open_close_menu() {
+                if (!this.menu) {
+                    this.menu = true;
+                } else {
+                    this.menu = false;
+                }
+            }
+        },
+        created() {
+            if(document.documentElement.clientWidth > 700) {
+                this.menu = true;
+            } else {
+                this.menu = false;
+            }
+        }
     }
 </script>
 
@@ -30,7 +64,7 @@
         margin: 0 auto;
     }
 
-    nav a {
+    .nav-wrapper a {
         font-size: 0.75rem;
         font-weight: 400;
         line-height: 14px;
@@ -40,17 +74,60 @@
         margin-right: 30px;
     }
 
-    nav a:last-child {
+    .nav-wrapper a:last-child {
         margin-right: 0;
     }
 
-    nav a.router-link-exact-active {
+    .nav-wrapper a.router-link-exact-active {
         color: var(--Cyan);
+    }
+
+    .mobile-menu-btn {
+        display: none;
     }
 
     @media (max-width: 1200px) {
         header {
             padding: 4rem 2.5rem 3.375rem 2.5rem;
+        }
+    }
+
+    @media(max-width: 700px) {
+        header {
+            position: relative;
+        }
+        .mobile-menu {
+            position: absolute;
+            background-color: var(--GrayDarkBlue);
+            height: 200px;
+            top: 100%;
+            right: 25px;
+            padding: 35px 55px;
+            z-index: 2;
+        }
+        .nav-wrapper {
+            flex-direction: column;
+            justify-content: center;
+            margin-top: 55px;
+        }
+        .nav-wrapper a {
+            margin-right: 0;
+            margin-bottom: 25px;
+            color: white;
+        }
+        .mobile-menu-btn {
+            position: absolute;
+            top: 35%;
+            right: 25px;
+        }
+        .mobile-menu-btn {
+            display: block;
+            background: none;
+            border: none;
+            cursor: pointer;
+        }
+        .disabled {
+            display: block;
         }
     }
 </style>
---
+++ src/components/icons/Menu.vue
@@ -0,0 +1,19 @@
+<template>
+    <svg width="40px" height="40px" viewBox="0 0 24 24" xmlns="http://www.w3.org/2000/svg" fill="#33323D">
+        <g clip-path="url(#clip0_429_11066)">
+            <path fill="#33323D" d="M3 6.00092H21M3 12.0009H21M3 18.0009H21" stroke="#33323D" stroke-width="2.5" stroke-linecap="round"
+                stroke-linejoin="round" />
+        </g>
+        <defs>
+            <clipPath id="clip0_429_11066">
+                <rect  width="24" height="24" fill="#33323D" transform="translate(0 0.000915527)" />
+            </clipPath>
+        </defs>
+    </svg>
+</template>
+
+<script>
+    export default {
+        name: 'hamburguer-menu',
+    }
+</script>
